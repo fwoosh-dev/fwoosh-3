7703b1f3c15fe3cdedeb575d12941c75753d88af
Andrew Lisowski <lisowski54@gmail.com>
2024-06-26 21:21:55 -0700

    fix source panel crash

--- packages/plugin-source-panel/src/SourcePanel.tsx
+++ packages/plugin-source-panel/src/SourcePanel.tsx
@@ -11,22 +11,13 @@ const styles = stylex.create({
   },
 });
 
-export default async function SourcePanel({ page, story }: StoryContext) {
+export default async function SourcePanel({ page }: StoryContext) {
   const contents = await fs.readFile(page.file, "utf-8");
-  const lines = contents.split("\n");
-  const start =
-    lines.findIndex((i) => i.startsWith(`export const ${story.name} = `)) + 1;
-  const end = lines[start]?.includes("{")
-    ? lines.slice(start).findIndex((i) => i.match(/^};?$/))
-    : start;
-  const highlightLines = new Array(end - start + 1)
-    .fill(start)
-    .map((base, i) => base + i);
   const html = await highlightSyntax(contents, {
-    highlightLines,
     lang: path.extname(page.file).slice(1),
   });
 
+  console.log({ html });
   return (
     <div
       dangerouslySetInnerHTML={{ __html: html }}
